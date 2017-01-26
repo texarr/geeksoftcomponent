73478c2561bc38eaa84b9ded10cf3f7ad6f0f3e9
texarr <daniel.pietrucha@hotmail.com>
2017-01-26 11:11:18 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  return gulp.src('sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle:'compressed', sourceComments: 'map'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch('css/**/*.css').on('change', browserSync.reload);
+  gulp.watch('js/*.js').on('change', browserSync.reload);
+});
